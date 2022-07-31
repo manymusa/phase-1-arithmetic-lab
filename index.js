d1e07db50769916a3ab8bc1957f9436ae5870b7b
manymusa <adanalin113@gmail.com>
2022-07-31 19:38:23 -0400

    commit tree ade0bd05524b203889e5dd714a2d63e021f4113e

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 53
+const num4 = 7;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() + 1);
+
+const mod = 53%7;
+
+const max = Math.max(20,19,18,17);
 
 // Write your code here
